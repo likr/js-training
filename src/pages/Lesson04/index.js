32ceb2fa9ebde225b37a617355ef19a4319013f8
Tsubame <watamisa08130505@outlook.jp>
2020-04-23 02:47:13 +0900

    2020/4/22

--- src/pages/Lesson04/index.js
+++ src/pages/Lesson04/index.js
@@ -3,8 +3,35 @@ import LessonPage from "../../components/LessonPage";
 import Chart from "../../components/Chart04";
 import instruction from "./instruction.md";
 
+/*
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  const obj = [
+    {
+      id: String,
+      data: [
+        {
+          x: Number,
+          y: Number,
+        },
+      ],
+    },
+  ];
+  return obj; // ここを作りましょう！
+};*/
+
+const convertData = (input) => {
+  // setはオブジェクトダメだから配列に←setで重複してるのを省く←mapで種類名だけにしたやつ
+  const species = Array.from(new Set(input.map(({ species }) => species)));
+  //種類ごとでreturnしたい
+  return species.map((species) => {
+    //中身
+    return {
+      id: species,
+      data: input //inputのデータの中でspeciesが一致するものをfilterでひろう
+        .filter((item) => item.species === species)
+        .map(({ sepalLength: x, petalWidth: y }) => ({ x, y })),
+    };
+  });
 };
 
 const Lesson = () => {
