a785c11308136ea7a8817c5cc0e7908f83c8d270
Tsubame <watamisa08130505@outlook.jp>
2020-04-24 23:04:49 +0900

    2020/4/24

--- src/pages/Lesson06/index.js
+++ src/pages/Lesson06/index.js
@@ -4,8 +4,44 @@ import Chart from "../../components/Chart06";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  const data = Array.from(
+    input.map(({ gender, y, x }) => {
+      let col;
+      if (gender === "男性") {
+        col = "blue";
+      } else {
+        col = "red";
+      }
+      const obj = {
+        color: col,
+        gender: gender,
+        bmi: (x / (y * y)) * 10000,
+        weight: x,
+        height: y,
+      };
+      return obj;
+    })
+  );
+  return data;
 };
+
+//模範解答
+/*
+const convertData = (input) =>{
+  const colors = { //colorでgenderをkeyで取れるようにobj作る
+    男性: "blue",
+    女性: "red"
+  };
+  return input.map(({gender, x, y})=>{
+    return{
+      color: colors[gender],
+      gender,
+      bmi: x/(y/100)**2,
+      weight: x,
+      height: y,
+    };
+  });
+};*/
 
 const Lesson = () => {
   return (
