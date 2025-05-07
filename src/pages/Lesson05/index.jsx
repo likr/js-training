04f11cb35aacfbf243d4eadf4b94cb313c465d7e
minmmmmin <kotomi.hinahima@gmail.com>
2025-05-07 21:54:41 +0900

    Lesson05何とか理解

--- src/pages/Lesson05/index.jsx
+++ src/pages/Lesson05/index.jsx
@@ -4,7 +4,23 @@ import Chart from "../../components/Chart05";
 import instruction from "./instruction.md?raw";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  const genders = Array.from(new Set(input.map(({ gender }) => gender)));
+  const min = Math.round(Math.min(...input.map(({ y }) => y)));
+  const max = Math.round(Math.max(...input.map(({ y }) => y)));
+  const bins = Array.from({ length: max - min + 1 }).map((_, i) => {
+    const obj = {
+      bin: (min + i).toString(),
+    };
+    for (const gender of genders) {
+      obj[gender] = 0;
+    }
+    return obj;
+  });
+  for (const { y, gender } of input) {
+    const i = Math.round(y) - min;
+    bins[i][gender] += 1;
+  }
+  return bins;
 };
 
 const Lesson = () => {
